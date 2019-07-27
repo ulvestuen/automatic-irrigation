90ef35903ed485bb5d17c28d5cb2a9d31b6e6fc7
Idar Ulvestad Haugstuen <idaruh@gmail.com>
2019-07-27 17:30:59 +0200

    ISSUE-5: Added support for OTA firmware update, and updated README.md with example.

--- src/api/IrrigationSystemApi.h
+++ src/api/IrrigationSystemApi.h
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
 #include <model/IrrigationSystemStatus.h>
 #include <service/IrrigationSystemService.h>
 
@@ -11,6 +12,7 @@ class IrrigationSystemApi
 
 private:
   ESP8266WebServer *server;
+  ESP8266HTTPUpdateServer *httpUpdater;
   IrrigationSystemStatus *systemStatus;
   IrrigationSystemService *systemService;
   std::vector<int> toVector(String stringWithDelimiter, std::string delimiter);
